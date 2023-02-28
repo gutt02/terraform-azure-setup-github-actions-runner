672e928c024968438272bc7e0ef03a3ddfb11836
gutt02 <sven.guttmann@gmail.com>
2023-02-28 17:15:41 +0100

    typo

--- modules/github-actions-runner/scripts/gh_runner.sh
+++ modules/github-actions-runner/scripts/gh_runner.sh
@@ -6,6 +6,7 @@
 sudo su - ghrund -c "mkdir actions-runner-<project>"
 sudo su - ghrund -c "cd actions-runner-<project> && curl -o actions-runner-linux-x64-2.301.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.301.1/actions-runner-linux-x64-2.301.1.tar.gz"
 sudo su - ghrund -c "cd actions-runner-<project> && tar xzf ./actions-runner-linux-x64-2.301.1.tar.gz"
-sudo su - ghrund -c "cd actions-runner-<project> && ./config.sh --url https://github.com/gutt02/<REPRO> --token <TOKEN>"
+sudo su - ghrund -c "cd actions-runner-<project> && rm -f ./actions-runner-linux-x64-2.301.1.tar.gz"
+sudo su - ghrund -c "cd actions-runner-<project> && ./config.sh --url https://github.com/gutt02/<REPO> --token <TOKEN>"
 sudo su -c "cd /home/ghrund/actions-runner-<project> && ./svc.sh install ghrund"
 sudo su -c "cd /home/ghrund/actions-runner-<project> && ./svc.sh start"
